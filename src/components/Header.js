2be3655f79e77e08da1cffda4290b32c86a2e285
KeshavBansal05 <keshav4788.be23@chitkara.edu.in>
2024-10-23 18:59:48 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';  // Make sure to create a separate CSS file for styling
+
+function Header() {
+  return (
+    <header className="site-header">
+      <h1>DEV@Deakin</h1>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+        <button className="btn post">Post</button>
+        <button className="btn login">Login</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
